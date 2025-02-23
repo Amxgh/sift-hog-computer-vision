99bfd9fb3d6cd45efd78b2e9fb232b602b7a2643
Amogh <axm0754@mavs.uta.edu>
2025-02-23 04:42:01 -0600

    Add image processing function to extract HOG features

--- feature_extraction.py
+++ feature_extraction.py
@@ -9,6 +9,32 @@ from skimage.transform import resize
 
 # TODO: Create feature processing functions for SIFT and HOG
 
+def process_images(images):
+    features_list = []
+
+    for x in images:
+        image_arr = np.asarray(x).reshape((32, 32, 3))
+        gray_image = rgb2gray(image_arr)
+
+        h, w = gray_image.shape
+        ratio = w / h
+        new_h = 300
+        new_w = int(new_h * ratio)
+
+        resized_image = resize(gray_image, (new_h, new_w))
+
+        # TODO: Call HOG
+        grad_norms, angles = calculate_grad_norms(resized_image)
+
+        # Extract features for multiple window positions
+        for i in range(0, resized_image.shape[0] - 16, 16):
+            for j in range(0, resized_image.shape[1] - 16, 16):
+                features = create_hog(angles, grad_norms, (i + 8, j + 8))
+                features_list.append(features)
+
+    return np.array(features_list)
+
+
 if __name__ == "__main__":
     # Load the pre-split data
     data = np.load("cifar10.npz", allow_pickle=True)
@@ -19,8 +45,12 @@ if __name__ == "__main__":
     X_test = data["X_test"]
     y_test = data["y_test"]
 
-    print(X_train[0])
 
     # TODO: Extract features from the testing data
+    train_features = process_images(X_train)
+    test_features = process_images(X_test)
 
     # TODO: Save the extracted features to a file
+
+    np.savez("features_hog.npz", X_train_features=train_features, y_train=y_train,
+             X_test_features=test_features, y_test=y_test)

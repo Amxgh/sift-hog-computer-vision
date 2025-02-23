6a0ff57b0410fb140f04023f9d44c2b23f722ae6
Amogh <axm0754@mavs.uta.edu>
2025-02-23 04:42:47 -0600

    Add function to calculate gradient norms and angles for image processing

--- feature_extraction.py
+++ feature_extraction.py
@@ -9,6 +9,43 @@ from skimage.transform import resize
 
 # TODO: Create feature processing functions for SIFT and HOG
 
+def create_hog(angles, grad_norms, center, window_size=16):
+    # Create HOG vector for a given window
+    window_half = int(window_size / 2)
+    window_angles = angles[center[0] - window_half:center[0] + window_half,
+                    center[1] - window_half:center[1] + window_half]
+    window_norms = grad_norms[center[0] - window_half:center[0] + window_half,
+                   center[1] - window_half:center[1] + window_half]
+
+    # Normalize angles
+    window_angles[window_angles < 0] += np.pi
+
+
+    # Remove NaN values
+    window_angles = np.nan_to_num(window_angles)
+    window_norms = np.nan_to_num(window_norms)
+
+    grid_size = window_half
+    hog_vector = np.array([])
+
+    for i in range(2):
+        for j in range(2):
+            cell_angles = window_angles[i * grid_size:i * grid_size + grid_size,
+                          j * grid_size:j * grid_size + grid_size]
+            cell_norms = window_norms[i * grid_size:i * grid_size + grid_size,
+                         j * grid_size:j * grid_size + grid_size]
+
+            hist, _ = np.histogram(cell_angles, bins=9, weights=cell_norms)
+            hog_vector = np.append(hog_vector, hist)
+
+
+    # Normalization
+    hog_vector = hog_vector / np.linalg.norm(hog_vector)
+    hog_vector[hog_vector > 0.2] = 0.2
+
+    return hog_vector / np.linalg.norm(hog_vector)
+
+
 def calculate_grad_norms(image, sigma=1.6):
     dx = np.array([[-1, 0, 1],
                    [-2, 0, 2],

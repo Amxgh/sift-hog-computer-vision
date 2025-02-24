3c6a9bf66b37a4649c9e8d182fbbe7602b6371eb
Amogh <axm0754@mavs.uta.edu>
2025-02-23 18:08:35 -0600

    Add SIFT descriptor matching function and integrate SIFT feature extraction

--- feature_extraction.py
+++ feature_extraction.py
@@ -3,9 +3,22 @@ import scipy.ndimage as ndi
 from skimage import filters
 from skimage.color import rgb2gray
 from skimage.transform import resize
+from skimage.feature import SIFT
+
 
 
 # TODO: Create feature processing functions for SIFT and HOG
+
+def match_sift_descriptors(desc1, desc2, ratio_threshold=0.75):
+    matches = []
+    for i, d1 in enumerate(desc1):
+        distances = np.linalg.norm(desc2 - d1, axis=1)
+        sorted_idx = np.argsort(distances)
+        # Ensure there are at least two descriptors to compare
+        if len(distances) > 1 and distances[sorted_idx[0]] < ratio_threshold * distances[sorted_idx[1]]:
+            matches.append((i, sorted_idx[0]))
+    return matches
+
 
 def create_hog(angles, grad_norms, center, window_size=16):
     # Create HOG vector for a given window
@@ -70,6 +83,10 @@ def process_images(images):
     for x in images:
         image_arr = np.asarray(x).reshape((32, 32, 3))
         gray_image = rgb2gray(image_arr)
+        sift = SIFT()
+        sift.detect_and_extract(gray_image)
+        keypoints1 = sift.keypoints
+        descriptors1 = sift.descriptors
 
         h, w = gray_image.shape
         ratio = w / h
@@ -93,18 +110,16 @@ if __name__ == "__main__":
     # Load the pre-split data
     data = np.load("cifar10.npz", allow_pickle=True)
 
-    # TODO: Extract features from the training data
+    # Extract features from the training data
     X_train = data["X_train"].astype(np.uint8)
     y_train = data["y_train"]
     X_test = data["X_test"]
     y_test = data["y_test"]
 
-
-    # TODO: Extract features from the testing data
+    # Extract features from the testing data
     train_features = process_images(X_train)
     test_features = process_images(X_test)
 
-    # TODO: Save the extracted features to a file
-
+    # Save the extracted features to a file
     np.savez("features_hog.npz", X_train_features=train_features, y_train=y_train,
              X_test_features=test_features, y_test=y_test)
